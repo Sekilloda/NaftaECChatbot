da2c266857c3f8a0f7911ed3d053855ada0974d3
Daniel Garcia-V <danielgarciavillacanas@protonmail.com>
2026-03-24 22:14:08 +0100

    Update dependencies

--- Local/core/knowledge.py
+++ Local/core/knowledge.py
@@ -5,7 +5,6 @@ import requests
 import pandas as pd
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
 from google import genai
 from google.genai import types
 from huggingface_hub import login
